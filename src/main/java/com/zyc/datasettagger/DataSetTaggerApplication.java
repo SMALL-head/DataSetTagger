6d95baa30ff6934b317e7f4e20b27fcf84d9a728
zyc <skyzyc@126.com>
2023-03-11 20:27:56 +0800

    create app

---
+++ src/main/java/com/zyc/datasettagger/DataSetTaggerApplication.java
@@ -0,0 +1,15 @@
+package com.zyc.datasettagger;
+
+import lombok.extern.slf4j.Slf4j;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@Slf4j
+@MapperScan("com.zyc.mapper")
+public class DataSetTaggerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DataSetTaggerApplication.class, args);
+    }
+}
---
+++ src/test/java/com/zyc/datasettagger/DataSetTaggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zyc.datasettagger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataSetTaggerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
